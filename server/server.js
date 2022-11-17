de3ab854e49b5a04d0c7a4c6d22f2de297c509ce
Charlie <c.alejandro.oso@gmail.com>
2022-11-16 16:11:51 -0800

    updating sockets

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const app = express();
 const cors = require('cors') 
 const port = 8000;
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 const http = require('http').createServer(app);
 http.listen(port, () => {
     console.log(`Listening on: ${port}`);
 });
 
-const io = require('socket.io')(http);
-io.on('connection', (socket) => {
+const socket = require('socket.io')(http);
+socket.on('connection', (socket) => {
     console.log('io connected');
 });
 
@@ -33,14 +33,14 @@ require("./routes/pet.routes")(app);
 
 const server = app.listen(port, () => console.log(`Listening on port: ${port}`) );
 
-// const io = socket(server, {
-//     cors: {
-//         origin: "*:*",
-//         methods: ['GET', 'POST'],
-//         allowedHeaders: ['*'],
-//         credentials: true,
-//     }
-// });
+const io = socket(server, {
+    cors: {
+        origin: "*:*",
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['*'],
+        credentials: true,
+    }
+});
 
 io.on("connection", socket => {
     console.log('socket id: ' + socket.id);
